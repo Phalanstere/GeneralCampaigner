92af4f9d0cf3751a765806daf08eefffc34adb37
Phalanstere <elias@ludicphilosophy.com>
2018-11-03 12:53:03 +0100

    linting rules

---
+++ index.js
@@ -0,0 +1,23 @@
+// MODULES //
+
+// VARIABLES //
+
+
+
+// MAIN //
+
+/**
+ * GeneralCampaigner - main module
+ */
+
+var GeneralCampaigner =  {
+
+
+	run: function() {
+		console.log("Campaigner");
+	}
+
+};
+
+
+GeneralCampaigner.run();
